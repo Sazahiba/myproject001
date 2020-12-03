83242ffb10122ddad2e34e312c1b9347b15f9457
Saba Shah <sabazahidshahbukhari@gmail.com>
2020-12-03 14:35:04 +0500

    fixed

--- src/Child.js
+++ src/Child.js
@@ -22,23 +22,6 @@ function Child() {
         setAmount(0)
     }
 
-    const getIncome = () => {
-        let income = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            if (transactions[i].amount > 0)
-                income = income + transactions[i].amount
-        }
-        return income;
-    }
-
-    const getExpense = () => {
-        let expense = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            if (transactions[i].amount < 0)
-                expense += transactions[i].amount
-        }
-        return expense;
-    }  
     
     return (
     <div className="container">
